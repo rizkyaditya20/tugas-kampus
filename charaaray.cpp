16806893f1459c79b4eed59292111b85ead97110
rizkyaditya20 <rizkyaditya9820@gmail.com>
2017-02-25 20:51:13 +0700

    Add files via upload

---
+++ charaaray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+// Program Fungsi Char Array
+// I.S Program mengolah angka
+// F.S Program Menampilkan Data Array
+
+int main() {
+    // Fungsi string
+    string nama = "ACEPAHMAD" ;
+    // Fungsi Char
+    char nama2[] =  "ACEPAHMAD" ;
+    char nama3[9] = {'A', 'C', 'E', 'P', 'A', 'H', 'M', 'A', 'D'};
+    // Menampilkan hasil data
+    cout << nama << endl;
+    cout << nama2 << endl;
+    cout << nama3[5] << endl; 
+
+// Akhir Program
+return 0;
+}
